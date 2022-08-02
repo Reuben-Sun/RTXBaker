69a3f4dd662403d8fa68f605c8c15216d6e596a1
Reuben-PC <2824016820@qq.com>
2022-08-02 20:45:16 +0800

    background

---
+++ RTXBaker/Packages/com.reuben.rtx-baker/Editor/Menu.cs
@@ -0,0 +1,24 @@
+﻿using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+
+namespace Reuben.RTXBaker.Editor
+{
+    public class MainMenuWindow: OdinMenuEditorWindow
+    {
+        protected override OdinMenuTree BuildMenuTree()
+        {
+            var tree = new OdinMenuTree();
+            tree.Add("Baker Panel", new BakePanel(), EditorIcons.House);
+            return tree;
+        }
+
+        [MenuItem("Tools/RTX Baker Menu")]
+        private static void OpenWindow()
+        {
+            var window = GetWindow<MainMenuWindow>();
+            window.position = GUIHelper.GetEditorWindowRect().AlignCenter(800, 600);
+        }
+    }
+}
