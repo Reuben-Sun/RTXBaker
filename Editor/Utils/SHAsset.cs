4874e70457e6fece55e275af2114fb0f8b5c4c68
Reuben_Sun <2824016820@qq.com>
2022-08-08 17:42:48 +0800

    Save in asset

--- Editor/Utils/SHAsset.cs
+++ Editor/Utils/SHAsset.cs
@@ -5,6 +5,6 @@ namespace Reuben.RTXBaker.Editor
 {
     public class SHAsset: ScriptableObject
     {
-        public List<SH9Color> _SH9Color = new List<SH9Color>();
+        public List<Vector4> SH9Color = new List<Vector4>();
     }
 }
